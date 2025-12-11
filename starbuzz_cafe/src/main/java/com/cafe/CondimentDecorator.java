bb72be6313a52c10040184ca98bacd2867d16c36
Govindraj Ingle <govindrajingle@gmail.com>
2025-12-11 22:28:34 +0530

    decorator pattern

---
+++ starbuzz_cafe/src/main/java/com/cafe/CondimentDecorator.java
@@ -0,0 +1,5 @@
+package com.cafe;
+
+public abstract class CondimentDecorator extends Beverage {
+    public abstract String getDescription();
+}
---
+++ starbuzz_cafe/src/main/java/com/cafe/Main.java
@@ -0,0 +1,11 @@
+package com.cafe;
+
+public class Main {
+    public static void main(String[] args) {
+        Beverage beverage = new Espresso();
+        System.out.println(beverage.getDescription() + " $" + beverage.cost());
+        beverage = new Mocha(beverage);
+        beverage = new Mocha(beverage);
+        System.out.println(beverage.getDescription() + " $" + beverage.cost());
+    }
+}
